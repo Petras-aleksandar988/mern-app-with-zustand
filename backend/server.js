510ffa48351c2c281f16be274586dabdd022a29b
aco <petras.aleksandar49@gmail.com>
2024-10-02 14:51:42 +0200

    adding build and start command so api and frontend run on same port

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import products from './routes/products.js';
-
 import cors from 'cors'
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 import connectDB from './database/mongoDB.js';
-connectDB()
-
-const port = process.env.PORT || 5000;
-// Get the directory name
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 // Enable CORS for all origins
 app.use(cors());
@@ -28,14 +23,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
 // setup static folder
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/products', products);
 
 
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 // Error handler
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => {
+connectDB()
+console.log(`Server is running on port ${port}`)
+});
+    
